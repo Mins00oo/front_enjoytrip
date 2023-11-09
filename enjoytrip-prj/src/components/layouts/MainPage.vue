6cef7e1d047b4c464c05c812aeee27e757235af4
mins00oo <alstn7223@gmail.com>
2023-11-10 01:07:23 +0900

    router설정 main, about수정

---
+++ enjoytrip-prj/src/components/layouts/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <TourList></TourList>
+  <TourFeature></TourFeature>
+</template>
+
+<script>
+import TourList from '../tours/TourList.vue'
+import TourFeature from '../tours/TourFeature.vue'
+
+export default {
+  components: {
+    // 컴포넌트를 등록
+    TourList,
+    TourFeature
+  }
+}
+</script>
---
+++ enjoytrip-prj/src/views/AppMain.vue
@@ -0,0 +1,14 @@
+<template>
+  <tour-list></tour-list>
+  <tour-recommend></tour-recommend>
+</template>
+
+<script>
+import TourList from '@/components/tours/TourList.vue'
+import TourRecommend from '@/components/tours/TourRecommend.vue'
+import TourFeature from '@/components/tours/TourFeature.vue'
+
+export default {
+  name: 'AppMain'
+}
+</script>
