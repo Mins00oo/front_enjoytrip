b430b2688c6c6fa5c04f8536809a1ac7e594853f
RaelJung <allice2283006@gmail.com>
2023-11-21 09:16:58 +0900

    mytrip 목록 조회

---
+++ enjoytrip-project/src/components/mytrip/MyTripList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div v-for="(mytrip, index) in dataList" :key="index" class="mytrip-container">
+            <div class="review-tour">{{ mytrip.title }}</div>
+            <div class="review-score">{{ review.score }}</div>
+            <div class="review-content">{{ review.content }}</div>
+            <div class="review-author">작성자: {{ review.userNickname }}</div>
+            <i @click="deleteReview(review.reviewId)" class="fas fa-trash"></i>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import http from '@/common/axios.js'
+
+//리뷰 정보 가져오기
+const dataList = ref([])
+const list = async () => {
+    try {
+        let { data } = await http.get("/tours/reviews");
+        dataList.value = data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+list()
+
+const deleteReview = async (reviewId) => {
+    try {
+        let { data } = await http.delete("/tours/reviews/" + reviewId);
+        console.log(data)
+        list()
+    } catch (error) {
+        console.error(error);
+    }
+    list()
+}
+</script>
+
+<style scoped>
+@import '../../assets/css/reivew.css';
+</style>
